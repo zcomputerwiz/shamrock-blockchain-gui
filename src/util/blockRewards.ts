2db7c6aa47ed880e5765eae7771f12078b5cc98b
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2021-12-04 10:14:37 -0600

    make block rewards match

--- src/util/blockRewards.ts
+++ src/util/blockRewards.ts
@@ -5,40 +5,14 @@ const BLOCKS_PER_YEAR = 1681920;
 
 export function calculatePoolReward(height: number): Big {
   if (height === 0) {
-    return clover_PER_shamrock.times(21000000).times(7 / 8);
+    return clover_PER_shamrock.times(0).times(7 / 8);
   }
-  if (height < 3 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(2).times(7 / 8);
-  }
-  if (height < 6 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(1).times(7 / 8);
-  }
-  if (height < 9 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(0.5).times(7 / 8);
-  }
-  if (height < 12 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(0.25).times(7 / 8);
-  }
-
-  return clover_PER_shamrock.times(0.125).times(7 / 8);
+  return clover_PER_shamrock.times(2).times(7 / 8);
 }
 
 export function calculateBaseFarmerReward(height: number): Big {
   if (height === 0) {
-    return clover_PER_shamrock.times(21000000).times(1 / 8);
+    return clover_PER_shamrock.times(0).times(1 / 8);
   }
-  if (height < 3 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(2).times(1 / 8);
-  }
-  if (height < 6 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(1).times(1 / 8);
-  }
-  if (height < 9 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(0.5).times(1 / 8);
-  }
-  if (height < 12 * BLOCKS_PER_YEAR) {
-    return clover_PER_shamrock.times(0.25).times(1 / 8);
-  }
-
-  return clover_PER_shamrock.times(0.125).times(1 / 8);
+  return clover_PER_shamrock.times(2).times(1 / 8);
 }
